3bcb9df5b2f7700180c81b7275846eb0797073d4
Preston Smith <preston.s1993@gmail.com>
2021-05-30 03:07:40 -0500

    JConfig and Cache

---
+++ src/JConfig.java
@@ -0,0 +1,98 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JConfig {
+    // Official client version information, subversion uses 'other_sub_version'
+    public static final int VERSION = 124;
+    public static final int SUBVERSION = 2;
+
+    public Map<String, String> parameters = new HashMap<>();
+
+    private Map<String, String> m_data = new HashMap<>();
+
+    public boolean fetch(String url) {
+        try {
+            URL configURL = new URL(url);
+
+            // Open connection
+            URLConnection connection = configURL.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String line;
+            while ((line = in.readLine()) != null) {
+                // Skip empty lines
+                if (line.length() <= 0) continue;
+
+                String key = line.substring(0, line.indexOf('='));
+
+                // Skip official client locale messages
+                if ("msg".equals(key)) continue;
+
+                String value = line.substring(key.length() + 1);
+
+                switch (key) {
+                    case "param":
+                        String paramKey = value.substring(0, value.indexOf('='));
+                        String paramValue = value.substring(paramKey.length() + 1);
+                        parameters.put(paramKey, paramValue);
+                        break;
+                    default:
+                        m_data.put(key, value);
+                        break;
+                }
+            }
+
+            // Close connection
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean isSupported() {
+        String version = m_data.get("viewerversion");
+        if (version == null) version = "124";
+
+        String subVersion = m_data.get("other_sub_version");
+        if (subVersion == null) subVersion = "2";
+
+        return Integer.valueOf(version) <= VERSION && Integer.valueOf(subVersion) <= SUBVERSION;
+    }
+
+    public String getString(String key) {
+        if (m_data.containsKey(key))
+            return m_data.get(key);
+        else
+            return null;
+    }
+
+    public String getParameter(String key) {
+        if (parameters.containsKey(key))
+            return parameters.get(key);
+        else
+            return null;
+    }
+
+    public URL getURL(String key) {
+        try {
+            return new URL(m_data.get(key));
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public int getInteger(String key) {
+        String string = m_data.get(key);
+        if (string != null) return Integer.valueOf(string);
+        else return 0;
+    }
+}
--- src/launcher.java
+++ src/launcher.java
@@ -17,37 +17,13 @@ public class launcher extends JFrame implements AppletStub, AppletContext, Windo
     private static launcher instance;
     private HashMap<String, String> parameters = new HashMap<String, String>();
     private Applet applet;
+    JConfig config;
 
     public static void main(String[] args) {
         instance = new launcher();
     }
 
-    private void loadParameters() {
-        parameters.put("20", "https://token-auth.production.jxp.aws.jagex.com/");
-        parameters.put("19", "196515767263-1oo20deqm6edn7ujlihl6rpadk9drhva.apps.googleusercontent.com");
-        parameters.put("15", "0");
-        parameters.put("11", "https://auth.jagex.com/");
-        parameters.put("8", "true");
-        parameters.put("4", "58793");
-        parameters.put("5", "1");
-        parameters.put("12", "490");
-        parameters.put("14", "0");
-        parameters.put("13", ".runescape.com");
-        parameters.put("2", "https://payments.jagex.com/");
-        parameters.put("9", "ElZAIrq5NpKN6D3mDdihco3oPeYN2KFy2DCquj7JMmECPmLrDP3Bnw");
-        parameters.put("18", "");
-        parameters.put("1", "1");
-        parameters.put("10", "5");
-        parameters.put("16", "false");
-        parameters.put("6", "0");
-        parameters.put("7", "0");
-        parameters.put("3", "true");
-        parameters.put("17", "http://www.runescape.com/g=oldscape/slr.ws?order=LPWM");
-    }
-
     public launcher() {
-        loadParameters();
-
         applet = new client();
         applet.setStub(this);
         applet.setPreferredSize(new Dimension(800, 600));
@@ -61,8 +37,12 @@ public class launcher extends JFrame implements AppletStub, AppletContext, Windo
         pack();
 
         System.setProperty("user.home", ".");
-        applet.init();
-        applet.start();
+
+        config = new JConfig();
+        if (config.fetch("http://oldschool190.runescape.com/jav_config.ws") && config.isSupported()) {
+            applet.init();
+            applet.start();
+        }
     }
 
     @Override
@@ -77,20 +57,12 @@ public class launcher extends JFrame implements AppletStub, AppletContext, Windo
 
     @Override
     public URL getCodeBase() {
-        try {
-            return new URL("http://oldschool190.runescape.com/");
-        } catch (Exception e) {
-            return null;
-        }
+        return config.getURL("codebase");
     }
 
     @Override
     public String getParameter(String name) {
-        String result = null;
-        if (parameters.containsKey(name))
-            result = parameters.get(name);
-        System.out.println(name + ": " + result);
-        return result;
+        return config.getParameter(name);
     }
 
     @Override
@@ -124,12 +96,10 @@ public class launcher extends JFrame implements AppletStub, AppletContext, Windo
 
     @Override
     public void showDocument(URL url) {
-        showDocument(url, "");
     }
 
     @Override
     public void showDocument(URL url, String target) {
-        System.out.println("showDocument");
     }
 
     @Override
